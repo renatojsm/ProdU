9a74eceae512dc06e7abd48f283295153a36c9e2
Renato Mauricio <renato.santos.mauricio@gmail.com>
2024-06-17 23:11:40 +0100

    database models

---
+++ database/migrations/2024_06_17_215956_create_verification_questions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVerificationQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('verification_questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('question');
+            $table->foreignId('verification_categories_id')->constrained('verification_categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('verification_questions');
+    }
+}
---
+++ database/migrations/2024_06_17_220159_create_verification_answers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVerificationAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('verification_answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('verification_questions_id')->constrained('verification_questions')->onDelete('cascade');
+            $table->enum('tipo', ['Preliminar', 'Final']);
+            $table->enum('valor', ['Sim', 'Nao', 'N\A']);
+            $table->text('observacao')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('verification_answers');
+    }
+}
